9f4e25ceb49e49ae1b8b1dea8a624d7cbcb8e203
Hansuja <hansujaigdtuwcseai@gmail.com>
2026-05-11 03:52:15 +0530

    defined all the errors

---
+++ src/errors.rs
@@ -0,0 +1,25 @@
+use thisError::Error;
+
+#[derive(Debug, Error)]
+pub enum SssError {
+    #[error("invalid threshold: need 1 <= t <= n , got t={threshold} , n={num_shares}")]
+    InvalidThreshold{
+        threshold:usize,
+        num_shares:usize,
+    },
+    #[error("insufficient shares: need {threshold} got {provided}")]
+    InsufficientShares {
+        threshold: usize,
+        provided: usize,
+    },
+    #[error("invalid share format: {reason}")]
+    InvalidShareFormat {
+        reason: String,
+    },
+    #[error:("reconstruction failed: {reason}")]
+    ReconstructionFailed{
+        reason:String,
+    }
+    #[error("secret must not be empty")]
+    EmptySecret,
+}
